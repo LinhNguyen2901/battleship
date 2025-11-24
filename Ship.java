8dbd289e2c84cbe46744a022a6f4668d820aeb74
srb23n <sofiaroseflaca@gmail.com>
2025-11-24 14:40:41 -0500

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -1,25 +1,46 @@
-public class Ship {
+public class Ship 
+{
     private int length;
     private int hits;
     private int startRow, startCol;
     private boolean horizontal;
 
-    public Ship(int length, int startRow, int startCol, boolean horizontal) {
-        this.length = length;
-        this.startRow = startRow;
-        this.startCol = startCol;
-        this.horizontal = horizontal;
-        this.hits = 0;
+    public Ship(int l, int r, int c, boolean h) 
+    {
+        length = l;
+        startRow = r;
+        startCol = c;
+        horizontal = h;
+        hits = 0;
     }
 
-    public void hit() { hits++; }
-    public boolean isSunk() { return hits >= length; }
+    public void hit() 
+    {
+         hits++; 
+    }
+    public boolean isSunk() 
+    { 
+        return hits >= length; 
+    }
 
-    public boolean occupies(int r, int c) {
-        for (int i = 0; i < length; i++) {
-            int rr = startRow + (horizontal ? 0 : i);
-            int cc = startCol + (horizontal ? i : 0);
-            if (rr == r && cc == c) return true;
+    public boolean occupies(int r, int c) // see if a ship is on this cell
+    {
+        for (int i = 0; i < length; i++) 
+        {
+            int currentRow = startRow;
+            int currentCol = startCol;
+            if(horizontal)
+            {
+                currentCol += i; // increment columns by i if horizontal
+            }
+            else
+            {
+                currentRow +=i; // increment row by i if  ship is not horizontal
+            }
+            if (currentRow == r && currentCol == c) // if the coordinates match, a ship is there
+            {
+                return true;
+            }
         }
         return false;
     }
